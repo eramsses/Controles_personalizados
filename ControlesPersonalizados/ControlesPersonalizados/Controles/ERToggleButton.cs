cbb07a5447548138cd5dfa5e0c0ba5328dedae10
Erick R. Rodríguez ASUS <47439411+eramsses@users.noreply.github.com>
2021-11-04 11:40:49 -0600

    Inicio

---
+++ ControlesPersonalizados/ControlesPersonalizados/Controles/ERToggleButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace ControlesPersonalizados.Controles
+{
+    class ERToggleButton : CheckBox
+    {
+        //Campos
+        private Color onBackColor = Color.MediumSlateBlue
+    }
+}
